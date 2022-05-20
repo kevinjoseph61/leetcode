d573c23d83ccaa3bfb3e176ba48bd23f4364a96c
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-05-20 11:11:49 +0530

    Time: 181 ms (21.54%), Space: 18.9 MB (26.15%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,5 @@
+import collections
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = collections.Counter(nums)
+        return list(zip(*freq.most_common()))[0][:k]
