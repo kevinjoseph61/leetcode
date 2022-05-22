5648a49c7cc522d12ffe51e273e50996ddea7a63
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-05-22 14:57:22 +0530

    Time: 99 ms (5.06%), Space: 14.4 MB (90.41%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        r = bisect_left(matrix, target, key=lambda row: row[-1])  # or key=itemgetter(-1)
+        return r < len(matrix) and matrix[r][bisect_left(matrix[r], target)] == target
+        
+        
