48482946e431a97114ea1a22c31009773f048d5c
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-05-29 11:53:28 +0530

    Time: 189 ms (19.06%), Space: 17.3 MB (14.18%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for i in prerequisites:
+            adj[i[0]].append(i[1])
+        complete = set()
+        visited = set()
+        
+        def dfs(i):
+            
+            if i in complete:
+                return True
+            if i in visited:
+                return False
+            visited.add(i)
+            for j in reversed(adj[i]):
+                if dfs(j):
+                    adj[i].remove(j)
+                else:
+                    raise KeyError
+            if len(adj[i]) == 0:
+                complete.add(i)
+                visited.remove(i)
+                return True
+            else:
+                raise KeyError
+        
+        for i in list(adj):
+            try:
+                if not dfs(i):
+                    return False
+            except:
+                return False
+        return True
+        
+            
+            
+            
