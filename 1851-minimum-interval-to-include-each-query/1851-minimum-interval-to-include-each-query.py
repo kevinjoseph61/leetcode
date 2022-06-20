a3c146ba7018dd5e635609fd8b845efcb1acae39
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-06-20 23:24:39 +0530

    Time: 2893 ms (31.77%), Space: 56.8 MB (29.39%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
@@ -9,17 +9,12 @@ class Solution:
         
         i = 0
         for j in q:
-            while(i < n and intervals[i][1] < j):
-                i += 1
             while(i < n and intervals[i][0] <= j ):
                 heapq.heappush(heap, (intervals[i][1] - intervals[i][0] + 1, intervals[i][1]))
                 i += 1
             while(heap and heap[0][1] < j):
                 heapq.heappop(heap)
-            if heap:
-                res[j] = heap[0][0]
-            else:
-                res[j] = -1
-        
+            res[j] = heap[0][0] if heap else -1
+
         return [res[k] for k in queries]
         
