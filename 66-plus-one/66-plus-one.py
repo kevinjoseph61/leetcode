35029fee22739673e68846736616a412cf41f21b
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-06-23 10:21:29 +0530

    Time: 55 ms (31.96%), Space: 13.9 MB (11.01%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,17 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         digits.reverse()
-        digits[0] += 1
-        carry = 0
         for i in range(len(digits)):
-            if carry == 1:
-                digits[i] += 1
+            digits[i] += 1
             if digits[i] >= 10:
                 digits[i] %= 10
-                carry = 1
             else:
-                carry = 0
-        if carry == 1:
+                break
+        else:
             digits.append(1)
         digits.reverse()
         return digits
