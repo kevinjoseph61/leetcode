10449acef7063997ebdd8a5c3c7699f550277c02
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-07-29 11:04:32 +0530

    Time: 52 ms (48.79%), Space: 13.9 MB (76.64%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        
+        def normalize(word):
+            d = {}
+            s = string.ascii_lowercase
+            i = 0
+            for w in word:
+                if w not in d:
+                    d[w] = s[i]
+                    i += 1
+            return ''.join([d[i] for i in word])
+        
+        p = normalize(pattern)
+        ans = []
+        for w in words:
+            if p == normalize(w):
+                ans.append(w)
+        return ans
