980d2ebce31613aaa18c5198e652443cd9f8ba75
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-09-04 13:49:22 +0530

    Time: 75 ms (7.82%), Space: 14.7 MB (5.42%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -4,17 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from sortedcontainers import SortedList
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        levels = defaultdict(list)
+        levels = defaultdict(SortedList)
         
         def traverse(node, levelh, levelv):
             if not node:
                 return
-            levels[levelh].append([levelv, node.val])
+            levels[levelh].add([levelv, node.val])
             traverse(node.left, levelh - 1, levelv + 1)
             traverse(node.right, levelh + 1, levelv + 1)
         
         traverse(root, 0, 0)
-        return [[y[1] for y in sorted(levels[x])] for x in sorted(levels)]
+        return [[y[1] for y in (levels[x])] for x in sorted(levels)]
         
