06322319426c05e358b5b06acae8c79959d6e36c
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-09-29 23:42:44 +0530

    Time: 69 ms (48.42%), Space: 13.9 MB (62.75%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def is_valid(self, nums, m, mid):
+        cuts, curr_sum  = 0, 0
+        for x in nums:
+            curr_sum += x
+            if curr_sum > mid:
+                cuts, curr_sum = cuts+1, x
+        subs = cuts + 1
+        return (subs <= m)
+    
+    def splitArray(self, nums: List[int], m: int) -> int:
+        low, high, ans = max(nums), sum(nums), -1
+        while low <= high:
+            mid = (low+high)//2
+            if self.is_valid(nums, m, mid): 
+                ans, high = mid, mid-1
+            else:
+                low = mid + 1
+        return ans
