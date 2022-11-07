3f3be4e9d6df476e3c3d05181ce481df80449a19
Kevin J. Joseph <kevinjoseph61@gmail.com>
2022-11-07 09:30:34 +0530

    Time: 36 ms (21.83%), Space: 13.3 MB (86.27%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maximum69Number (self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        s = list(str(num))
+        for i in range(len(s)):
+            if s[i] == '6':
+                s[i] = '9'
+                break
+        return "".join(s)
