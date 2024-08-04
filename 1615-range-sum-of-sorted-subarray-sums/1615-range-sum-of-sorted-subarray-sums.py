8f7d8b9ebba841321787b345b38a825afae4020b
Kevin J. Joseph <kevinjoseph61@gmail.com>
2024-08-04 14:57:55 +0530

    Time: 258 ms (33.63%), Space: 16.6 MB (97.79%) - LeetHub

---
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        MOD = 10 ** 9 + 7
+        minHeap = [(n, i) for i, n in enumerate(nums)]
+        heapq.heapify(minHeap)
+
+        res = 0
+        for i in range(right):
+            num, index = heapq.heappop(minHeap)
+            if i >= left - 1:
+                res = (res + num) % MOD
+            if index + 1 < n:
+                next_pair = (num + nums[index+1], index + 1)
+                heapq.heappush(minHeap, next_pair)
+        return res
